90af80f5fa7896e7832187deadd56e9997e2e6c9
lat9 <cindy@vinosdefrutastropicales.com>
2021-12-31 09:46:04 -0500

    #1: Cleaning up stray configuration settings.

--- YOUR_ADMIN/includes/init_includes/init_xsell_config.php
+++ YOUR_ADMIN/includes/init_includes/init_xsell_config.php
@@ -5,7 +5,7 @@
 // @copyright 2013 C Jones
 // $copyright 2021, lat9 (https://vinosdefrutastropicales.com).
 // @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
-// @version $Id: init_xsell_config.php v2.0.0, 2021-12-24, lat9 $
+// @version $Id: init_xsell_config.php v2.0.0, 2021-12-31, lat9 $
 //
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
@@ -71,17 +71,13 @@ if (!$result->EOF) {
         "INSERT INTO " . TABLE_CONFIGURATION . "
             (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function)
          VALUES
-            ('Display Cross-Sell Products Minimum', 'MIN_DISPLAY_XSELL', '1', 'This is the minimum number of configured Cross-Sell products required in order to cause the Cross Sell information to be displayed.<br>Default: 1', $cgi, 20, now(), NULL, NULL),
-
             ('Display Cross-Sell Products Maximum', 'MAX_DISPLAY_XSELL', '6', 'This is the maximum number of configured Cross-Sell products to be displayed.<br>Default: 6', $cgi, 25, now(), NULL, NULL),
 
-            ('Cross-Sell Products Columns per Row', 'SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS', '3', 'Cross-Sell Products Columns to display per Row<br>0= off or set the sort order.<br>Default: 3', $cgi, 30, now(), NULL, 'zen_cfg_select_option(array(\'0\', \'1\', \'2\', \'3\', \'4\', \'5\', \'6\'),'),
+            ('Cross-Sell Products Columns per Row', 'SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS', '3', 'Identify the number of cross-sells to display per row (on the storefront).  Set the value to <em>0</em> to display <em>all</em> products on a single row.  Default: <b>3</b>.', $cgi, 30, now(), NULL, NULL),
             
             ('Cross-Sell - Display prices?', 'XSELL_DISPLAY_PRICE', 'false', 'Cross-Sell &mdash; Do you want to display the product prices too?<br>Default: false', $cgi, 35, now(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),'),
 
-            ('Cross-Sell - Use common sort order?', 'XSELL_USE_COMMON_SORT_ORDER', 'false', 'Cross-Sell &mdash; Use per product sort order (on Catalog-&gt;Cross-Sell Admin) or common sort order (on Catalog-&gt;Categories/Products Edit) ?<br>Default: false', $cgi, 38, now(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),'),
-
-            ('Cross Sell Advanced Version', 'XSELL_VERSION', '0.0.0', 'Cross Sell Advanced Version (DO NOT MODIFY THIS VALUE!)', $cgi, 1, now(), NULL, NULL)"
+            ('Cross Sell Advanced II Version', 'XSELL_VERSION', '0.0.0', 'Current <em>Cross Sell Advanced II</em> Version', $cgi, 1, now(), NULL, , 'zen_cfg_read_only(')"
     );
 
     // -----
@@ -90,8 +86,8 @@ if (!$result->EOF) {
     $db->Execute(
         "CREATE TABLE IF NOT EXISTS " . TABLE_PRODUCTS_XSELL . "(
             ID int(11) NOT NULL auto_increment,
-            products_id int(11) NOT NULL DEFAULT 1,
-            xsell_id int(11) NOT NULL DEFAULT 1,
+            products_id int(11) NOT NULL,
+            xsell_id int(11) NOT NULL,
             sort_order int(11) NOT NULL DEFAULT 1,
             PRIMARY KEY (ID),
             KEY idx_products_id_xsell (products_id)
@@ -104,24 +100,74 @@ if (!$result->EOF) {
     zen_deregister_admin_pages(['configXsellCombo', 'catalogXSellComboAdmin', 'catalogXSellComboAdvancedAdmin']);
     zen_register_admin_page('configXsellCombo', 'BOX_CONFIGURATION_XSELL', 'FILENAME_CONFIGURATION', 'gID=' . $cgi, 'configuration', 'Y');
     zen_register_admin_page('catalogXSellComboAdmin', 'BOX_CATALOG_XSELL', 'FILENAME_XSELL', '', 'catalog', 'Y');
-    zen_register_admin_page('catalogXSellComboAdvancedAdmin', 'BOX_CATALOG_XSELL_ADVANCED', 'FILENAME_XSELL_ADVANCED', '', 'catalog', 'Y');
 
     define('XSELL_VERSION', '0.0.0');
 }
 
 if (XSELL_VERSION !== XSELL_CURRENT_VERSION) {
-    $db->Execute(
-        "UPDATE " . TABLE_CONFIGURATION . "
-            SET configuration_value = '" . XSELL_CURRENT_VERSION . "'
-          WHERE configuration_key = 'XSELL_VERSION'
-          LIMIT 1"
-    );
-
     switch (true) {
         // -----
-        // v2.0.0: On installation, provide some fixups from previous versions.
+        // v2.0.0: On installation, provide some fixups from previous versions and remove any previous "Cross Sell Advanced" tool
+        // from the admin menus.
         //
         case version_compare(XSELL_VERSION, '2.0.0', '<'):
+            // -----
+            // The functionality present in the previous "xsell_advanced" module is now provided in the base "xsell"; remove
+            // the previous module from the admin pages.
+            //
+            zen_deregister_admin_pages(['catalogXSellComboAdvancedAdmin']);
+
+            // -----
+            // Update configuration settings, removing those now obsolete and updating descriptions of those remaining.
+            //
+            $db->Execute(
+                "UPDATE " . TABLE_CONFIGURATION . "
+                    SET configuration_title = 'Cross Sell Advanced II Version',
+                        configuration_description = 'Current <em>Cross Sell Advanced II</em> Version',
+                        sort_order = 1,
+                        set_function = 'zen_cfg_read_only('
+                  WHERE configuration_key = 'XSELL_VERSION'
+                  LIMIT 1"
+            );
+            $db->Execute(
+                "DELETE FROM " . TABLE_CONFIGURATION . "
+                  WHERE configuration_key IN ('MIN_DISPLAY_XSELL', 'XSELL_USE_COMMON_SORT_ORDER')"
+            );
+            $db->Execute(
+                "UPDATE " . TABLE_CONFIGURATION . "
+                    SET configuration_description = 'Identify the maximum number of cross-sells to display on the storefront (default: <b>6</b>).<br><br>Set the value to <b>0</b> to disable the storefront display.'
+                  WHERE configuration_key = 'MAX_DISPLAY_XSELL'
+                  LIMIT 1"
+            );
+            $db->Execute(
+                "UPDATE " . TABLE_CONFIGURATION . "
+                    SET configuration_description = 'Identify the number of cross-sells to display per row (on the storefront).  Set the value to <em>0</em> to display <em>all</em> products on a single row.  Default: <b>3</b>.',
+                        set_function = NULL
+                  WHERE configuration_key = 'SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS'
+                  LIMIT 1"
+            );
+            $db->Execute(
+                "UPDATE " . TABLE_CONFIGURATION . "
+                    SET configuration_description = 'Should the cross-sell product prices be displayed on the storefront (default: \'false\')?'
+                  WHERE configuration_key = 'XSELL_DISPLAY_PRICE'
+                  LIMIT 1"
+            );
+
+            // -----
+            // Previous versions of "Cross Sell" plugins used 'unsigned int(10)' for the keys and provided a
+            // default value for the products_id and xsell_id fields.
+            //
+            // Extend all integer fields to 'int(11)', matching the 'products' table, and indicate that the
+            // products_id and xsell_id fields must be supplied on an insert.
+            //
+            $db->Execute(
+                "ALTER TABLE " . TABLE_PRODUCTS_XSELL . "
+                    MODIFY COLUMN `ID` int(11) NOT NULL auto_increment,
+                    MODIFY COLUMN products_id int(11) NOT NULL,
+                    MODIFY COLUMN xsell_id int(11) NOT NULL,
+                    MODIFY COLUMN sort_order int(11) NOT NULL DEFAULT 1"
+            );
+
             // -----
             // Remove duplicate entries in the 'products_xsell' table possibly allowed by previous "Cross Sell" plugins.
             //
@@ -164,6 +210,16 @@ if (XSELL_VERSION !== XSELL_CURRENT_VERSION) {
             break;
     }
 
+    // -----
+    // Update the plugin's version in the database and let the current admin know that the plugin's
+    // been installed or updated.
+    //
+    $db->Execute(
+        "UPDATE " . TABLE_CONFIGURATION . "
+            SET configuration_value = '" . XSELL_CURRENT_VERSION . "'
+          WHERE configuration_key = 'XSELL_VERSION'
+          LIMIT 1"
+    );
     if (XSELL_VERSION === '0.0.0') {
         $messageStack->add_session(sprintf(MESSAGE_XSELL_INSTALLED, XSELL_CURRENT_VERSION), 'success');
     } else {
--- includes/modules/xsell2_products.php
+++ includes/modules/xsell2_products.php
@@ -18,7 +18,6 @@
 // these constants.
 //
 if (!defined('MAX_DISPLAY_XSELL')) define('MAX_DISPLAY_XSELL', '6');
-if (!defined('MIN_DISPLAY_XSELL')) define('MIN_DISPLAY_XSELL', '1');
 if (!defined('XSELL_DISPLAY_PRICE')) define('XSELL_DISPLAY_PRICE', 'false');
 if (!defined('SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS')) define('SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS', '0');    //-If not set in admin, a value of '0' disables the display.
 
@@ -27,11 +26,10 @@ if (!defined('SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS')) define('SHOW_PRODUCT_I
 //
 $common_sort_order = (defined('XSELL_USE_COMMON_SORT_ORDER') && XSELL_USE_COMMON_SORT_ORDER === 'true');
 $xsell_max_display = (int)MAX_DISPLAY_XSELL;
-$xsell_min_display = (int)MIN_DISPLAY_XSELL;
 $xsell_columns = (int)SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS;
  
 // collect information on available cross-sell products for the current product-id
-if ($xsell_columns > 0 && $xsell_max_display > 0 && isset($_GET['products_id'])) {
+if ($xsell_columns >= 0 && $xsell_max_display > 0 && isset($_GET['products_id'])) {
     $xsell_query_sql = 
         "SELECT p.products_id, p.products_image, pd.products_name
            FROM " . TABLE_PRODUCTS_XSELL . " xp
@@ -43,7 +41,7 @@ if ($xsell_columns > 0 && $xsell_max_display > 0 && isset($_GET['products_id']))
           WHERE xp.products_id = " . (int)$_GET['products_id'] . "
             AND p.products_status = 1"; 
     if ($common_sort_order === true) { 
-        $xsell_query_sql .= " ORDER BY p.products_sort_order ASC LIMIT " . $xsell_max_display;
+        $xsell_query_sql .= " ORDER BY p.products_xsell_sort_order ASC LIMIT " . $xsell_max_display;
     } else { 
         $xsell_query_sql .= " ORDER BY xp.sort_order ASC LIMIT " . $xsell_max_display;
     }
@@ -51,43 +49,44 @@ if ($xsell_columns > 0 && $xsell_max_display > 0 && isset($_GET['products_id']))
     $xsell_query = $db->Execute($xsell_query_sql); 
     $num_products_xsell = $xsell_query->RecordCount();
 
-    // don't display if less than the minimum amount set in Admin->Config->Minimum Values->Cross-Sell
-    if ($xsell_min_display > 0 && $num_products_xsell >= $xsell_min_display) {
-        $row = 0;
-        $col = 0;
-        $list_box_contents = [];
-        $title='';
-        if ($xsell_columns === 0 || $num_products_xsell < $xsell_columns) {
-            $col_width = floor(100 / $num_products_xsell);
-        } else {
-            $col_width = floor(100 / $xsell_columns);
-        }
-        foreach ($xsell_query as $next_xsell) {
-            if ($common_sort_order) { 
-                $xsell_image = zen_image_OLD(DIR_WS_IMAGES . $next_xsell['products_image'], $next_xsell['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
-            } else { 
-                $xsell_image = zen_image(DIR_WS_IMAGES . $next_xsell['products_image'], $next_xsell['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT); 
-            }
-            $xsell_query_text =
-                '<a href="' . zen_href_link(zen_get_info_page($next_xsell['products_id']), 'products_id=' . $next_xsell['products_id']) . '">' . $xsell_image . '</a>' .
-                '<br>' .
-                '<a href="' . zen_href_link(zen_get_info_page($next_xsell['products_id']), 'products_id=' . $next_xsell['products_id']) . '">' . zen_output_string_protected($next_xsell['products_name']) . '</a>';
-            if (XSELL_DISPLAY_PRICE === 'true') {
-                $xsell_query_text .= '<br>' . zen_get_products_display_price($next_xsell['products_id']);
-            }
-
-            $list_box_contents[$row][$col] = [
-                'params' => 'class="centerBoxContentsCrossSell centeredContent back" style="width:' . $col_width . '%;"',
-                'text' => $xsell_query_text
-            ]; 
-
-            $col++;
-            if ($col > ($xsell_columns - 1)) {
-                $col = 0;
-                $row++;
-            }
-        }
-        // store data into array for display later where desired:
-        $xsell_data = $list_box_contents;
+    if ($num_products_xsell === 0) {
+        return;
     }
+
+    $row = 0;
+    $col = 0;
+    $list_box_contents = [];
+    $title = '';
+    if ($xsell_columns === 0 || $num_products_xsell < $xsell_columns) {
+        $col_width = floor(100 / $num_products_xsell);
+    } else {
+        $col_width = floor(100 / $xsell_columns);
+    }
+    foreach ($xsell_query as $next_xsell) {
+        if ($common_sort_order === true) { 
+            $xsell_image = zen_image_OLD(DIR_WS_IMAGES . $next_xsell['products_image'], $next_xsell['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
+        } else { 
+            $xsell_image = zen_image(DIR_WS_IMAGES . $next_xsell['products_image'], $next_xsell['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT); 
+        }
+        $xsell_query_text =
+            '<a href="' . zen_href_link(zen_get_info_page($next_xsell['products_id']), 'products_id=' . $next_xsell['products_id']) . '">' . $xsell_image . '</a>' .
+            '<br>' .
+            '<a href="' . zen_href_link(zen_get_info_page($next_xsell['products_id']), 'products_id=' . $next_xsell['products_id']) . '">' . zen_output_string_protected($next_xsell['products_name']) . '</a>';
+        if (XSELL_DISPLAY_PRICE === 'true') {
+            $xsell_query_text .= '<br>' . zen_get_products_display_price($next_xsell['products_id']);
+        }
+
+        $list_box_contents[$row][$col] = [
+            'params' => 'class="centerBoxContentsCrossSell centeredContent back" style="width:' . $col_width . '%;"',
+            'text' => $xsell_query_text
+        ]; 
+
+        $col++;
+        if ($col > ($xsell_columns - 1)) {
+            $col = 0;
+            $row++;
+        }
+    }
+    // store data into array for display later where desired:
+    $xsell_data = $list_box_contents;
 }
